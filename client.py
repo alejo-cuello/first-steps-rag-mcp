205ce595b4c9dd2202fbc71385ed2bd54e1f82ed
alejo-cuello <alejocuello.ib@gmail.com>
2025-10-25 10:34:20 -0300

    [add] server.py and client.py

---
+++ client.py
@@ -0,0 +1,104 @@
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+import os
+
+server_params = StdioServerParameters(
+    command="mcp",
+    params=["run","server.py"],
+    env=None
+)
+
+# def call_llm(prompt, functions):
+#     token = os.getenv("token") 
+#     endpoint = ""
+#     model_name = ""
+    
+#     client = ChatCompletionsClient(
+#         endpoint=endpoint,
+#         credential=AzureKeyCredential(token)
+#     )
+    
+#     response = client.complete(
+#         messages=[
+#             {
+#             "role": "system",
+#             "content": "You are a helpful assistant.",
+#             },
+#             {
+#             "role": "user",
+#             "content": prompt,
+#             },
+#         ],
+#         model=model_name,
+#         tools = functions,
+#         temperature=1.,
+#         max_tokens=150,
+#         top_p=1.    
+#     )
+
+#     response_message = response.choices[0].message
+    
+#     functions_to_call = []
+    
+#     if response.messagetool_calls:
+#         for tool_call in response_message.tool_calls:
+#             print("TOOL: ", tool_call)
+#             name = tool_call.function.name
+#             args = json.loads(tool_call.function.arguments)
+#             functions_to_call.append({ "name": name, "args": args })
+
+#     return functions_to_call
+
+# def convert_to_llm_tool(tool):
+#     tool_schema = {
+#         "type": "function",
+#         "function": {
+#             "name": tool.name,
+#             "description": tool.description,
+#             "type": "function",
+#             "parameters": {
+#                 "type": "object",
+#                 "properties": tool.inputSchema["properties"]
+#             }
+#         }
+#     }
+
+#     return tool_schema
+
+async def run():
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read,write) as session:
+            await session.initialize()
+            
+            # resources = await session.list_resources()
+            # for r in resources:
+            #     print(f"Resource {r.name}")
+                
+            tools = await session.list_tools()
+            for t in tools:
+                print(f"Resource {t.name}")
+                
+            # content, mimetype = await session.read_resource("greeting://hello")
+            # print(content, mimetype)
+            
+            # result = await session.read_tool("add", arguments={"a":1, "b":7})
+            # print(result.content)
+            
+            # functions = []
+            
+            # prompt = "Add 2 to 20"
+
+            # functions_to_call = call_llm(prompt, functions)
+
+            # for f in functions_to_call:
+            #     result = await session.call_tool(f["name"], arguments=f["args"])
+            #     print("TOOLS result: ", result.content)
+            
+                
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(run())
---
+++ server.py
@@ -0,0 +1,14 @@
+from mcp.server.fastmcp import FastMCP
+
+# Create a MCP server
+mcp = FastMCP("Demo")
+
+@mcp.tool()
+def add(a: int, b:int) -> int:
+    """Add two numbers"""
+    return a+b
+
+# @mcp.resource(f"greeting://{name}")
+# def get_greeting(name: str) -> str:
+#     """Get a personalized greeting"""
+#     return f"Hello, {name}"
